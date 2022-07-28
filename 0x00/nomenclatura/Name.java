d4c3fa8c1503a2bfbcb86c1d446ad035da72845f
thisiszizis <isisgf@ciandt.com>
2022-07-28 11:01:40 -0300

    Task 0 - Clean Code

---
+++ 0x00/nomenclatura/Name.java
@@ -0,0 +1,27 @@
+
+public class Name {
+
+    //A
+    public Date modifiedDate;
+    public List<Testcase> findAllTestcasesByUser(Name.User user){};
+
+    //A
+    public List<Account> AccountList;
+
+    //B
+    public void deleteUser(Name.User user){};
+
+    //B
+    public URL url;
+
+    //A
+    public class TableRepresent {};
+    public void saveCar(){};
+
+    //A
+    public saveUser(){}; saveAccount(){}; saveContract(){};
+
+    //B
+    public final int DAYS_IN_YEAR = 360;
+    public final int HOURS_IN_WEEK = 168;
+}
